bdc1b53530f1d74461c0ec0021cb2f8ca7880a02
Code_Geeks <harshita2000singh021@gmail.com>
2024-03-10 23:20:13 +0530

    Added New Project Custom Video Player

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from "react";
+import VideoContainer from "./container/VideoPLayer/VideoPlayer";
+import { VideoProvider } from "./contexts/videoContext";
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }}>
+      <VideoProvider>
+        <VideoContainer />
+      </VideoProvider>
     </div>
   );
-}
+};
 
 export default App;
